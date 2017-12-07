928a5c78e110c231c9a82361bbfc37b890fb1387
Bartosz Pietrzak <bartosz.pietrzak@seqr.com>
2017-12-07 19:02:30 +0100

    Last part of quantization parameters.

--- src/main/java/utils/calculator/SignalReconstructionParametersCalculator.java
+++ src/main/java/utils/calculator/SignalReconstructionParametersCalculator.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.commons.math.complex.Complex;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Iterables;
+
 import model.signal.base.Signal;
 
 /**
@@ -58,11 +60,38 @@ public class SignalReconstructionParametersCalculator
 
 	public Complex calculatePSNR(Signal signal)
 	{
-		return null;
+		List<Complex> signalValues = signal.getValues();
+		List<Complex> quantizationValues = signal.getQuantizationValues();
+		double maximum = Iterables.getFirst(signalValues, null).getReal();
+
+		double currentValue;
+		for (int i = 0; i < quantizationValues.size(); i++)
+		{
+			currentValue = quantizationValues.get(i).getReal();
+			if (currentValue > maximum)
+			{
+				maximum = currentValue;
+			}
+		}
+
+		return new Complex(10 * Math.log10(maximum / calculateMSE(signal).getReal()), 0.0D);
 	}
 
 	public Complex calculateMD(Signal signal)
 	{
-		return null;
+		List<Complex> signalValues = signal.getValues();
+		List<Complex> quantizationValues = signal.getQuantizationValues();
+
+		double maximum = Iterables.getFirst(signalValues, null).getReal() - Iterables.getFirst(quantizationValues, null).getReal();
+
+		for (int i = 0; i < quantizationValues.size(); i++)
+		{
+			if ((signalValues.get(i).getReal() - quantizationValues.get(i).getReal()) > maximum)
+			{
+				maximum = signalValues.get(i).getReal() - quantizationValues.get(i).getReal();
+			}
+		}
+
+		return new Complex(maximum, 0.0D);
 	}
 }
